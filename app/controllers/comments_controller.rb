ee8ce8f52d9e0ff587a7944c3bd847d729a45cd4
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-10 12:50:52 +0200

    Add action to create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @article = Article.comments.create(comment_params)
+    redirect_to article_pathe(@article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
