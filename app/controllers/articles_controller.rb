042f1a1f7fa8b76c8e46ccd95254dfc8d7e38dce
tresorsawasawa <tresorsawasawa@gmail.com>
2022-05-10 13:47:41 +0200

    Fix linters errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    
+
     if @article.update(article_params)
       redirect_to @article
     else
